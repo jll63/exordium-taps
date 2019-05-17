13af091d779034b68a84c6c0ea3c55bc4efb44b4
Jean-Louis Leroy <jleroy9@bloomberg.net>
2019-05-17 10:07:59 -0400

    exordium prefs

---
+++ exordium/after-init.el
@@ -0,0 +1,16 @@
+(setq exordium-spell-check nil
+      exordium-fci-mode :ondemand
+      exordium-enable-org-export nil)
+(add-hook 'c++-mode-hook 'fci-mode)
+
+(setq exordium-display-powerline-after-idle-time 2)
+
+(setq exordium-delete-trailing-whitespace t)
+
+;(setq exordium-enable-electric-pair-mode nil)
+
+(setq exordium-helm-everywhere t)
+
+(setq exordium-rtags-cmake t)
+
+(setq exordium-enable-insert-gui-primary-selection t)
