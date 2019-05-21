8aca556176878aadb43dc9bdca56ca57696872f1
Jean-Louis Leroy <jleroy9@bloomberg.net>
2019-05-21 16:12:51 -0400

    more tweaks

--- exordium/after-init.el
+++ exordium/after-init.el
@@ -7,10 +7,16 @@
 
 (setq exordium-delete-trailing-whitespace t)
 
-;(setq exordium-enable-electric-pair-mode nil)
+(setq exordium-enable-electric-pair-mode t)
 
 (setq exordium-helm-everywhere t)
 
 (setq exordium-rtags-cmake t)
 
 (setq exordium-enable-insert-gui-primary-selection t)
+
+(setq exordium-display-line-numbers nil)
+(setq exordium-rtags-auto-complete t)
+
+(setq exordium-powerline-theme :wave)
+(setq exordium-highlight-linum nil)
