97041122ab8de6a8a99b0d83b09dc4298d45ce65
Jean-Louis Leroy <jleroy9@bloomberg.net>
2019-08-22 09:33:29 -0400

    projectile: add generic project type

--- projectile/after-init.el
+++ projectile/after-init.el
@@ -2,3 +2,8 @@
 (global-set-key (kbd "C-S-t") 'projectile-test-project)
 (global-set-key (kbd "C-c p p") 'helm-projectile-switch-project)
 (global-set-key (kbd "C-c p f") 'helm-projectile)
+
+(projectile-register-project-type 'generic '("")
+                                  :compile "echo build"
+                                  :test "echo test"
+                                  :run "echo run")
