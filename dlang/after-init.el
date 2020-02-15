6da3ff322863db0962fb628504f9c203da56bf30
Jean-Louis Leroy <jl@leroy.nyc>
2020-02-14 22:14:28 -0500

    d-mode: indent after parenthesis

--- dlang/after-init.el
+++ dlang/after-init.el
@@ -4,3 +4,8 @@
           (lambda ()
             (electric-pair-mode)
             (electric-indent-mode)))
+
+(add-hook 'd-mode-hook
+          (lambda ()
+            (c-set-offset 'arglist-intro '+)
+            (c-set-offset 'arglist-close 0)))
