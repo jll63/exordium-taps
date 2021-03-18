28d6a78a80b10a6af887b9c6adfaec091851043c
Jean-Louis Leroy <jleroy9@bloomberg.net>
2021-03-18 09:08:07 -0400

    smart-compile pattern for python

--- smart-compile/after-init.el
+++ smart-compile/after-init.el
@@ -3,7 +3,9 @@
 (setq smart-compile-alist
       (list*
        '("\\.cpp$" . "clang++ -g -std=c++11 -o %n %f && ./%n")
-       '("\\.py\\'" . "python3.6 %f")
+       '("_test\\.py\\'" . "pytest %F")
+       '("test_.*\\.py\\'" . "pytest %F")
+       '("\\.py\\'" . "python %f")
        smart-compile-alist))
 
 (global-set-key [f12] 'smart-compile)
