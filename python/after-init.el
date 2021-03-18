22fbde14c0d1148c6b28cc75d12046e8f43d078b
Jean-Louis Leroy <jleroy9@bloomberg.net>
2021-03-18 09:07:54 -0400

    tweak elpy

--- python/after-init.el
+++ python/after-init.el
@@ -2,3 +2,9 @@
           (lambda ()
             (electric-pair-mode)
             (electric-indent-mode)))
+
+(use-package elpy
+  :ensure t
+  :defer t
+  :init
+  (advice-add 'python-mode :before 'elpy-enable))
